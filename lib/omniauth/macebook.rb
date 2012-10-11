2d3f3bda5b92ad00768ce7c1fa59f025c3521306
Sebastian Goodman <sebastian@stylesaint.com>
2012-10-11 16:53:16 -0700

    trying to debug options

--- lib/omniauth/macebook.rb
+++ lib/omniauth/macebook.rb
@@ -1,2 +1,3 @@
 require 'omniauth/macebook/version'
 require 'omniauth/strategies/macebook'
+require 'omniauth/mauth_code'
--- lib/omniauth/strategies/macebook.rb
+++ lib/omniauth/strategies/macebook.rb
@@ -87,8 +87,9 @@ module OmniAuth
 
       def build_access_token_from_code
         verifier = request.params['code']
+        overrides = {:redirect_uri => callback_url, 'client_secret' => nil}.merge(token_params.to_hash(:symbolize_keys => true))
         begin
-          mclient.auth_code.get_token(verifier, {:redirect_uri => callback_url, :client_secret => nil}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
+          mclient.auth_code.get_token(verifier, overrides, deep_symbolize(options.auth_token_params))
         rescue => e
           puts e.to_s
         end
